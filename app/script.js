84c7e9f8ea2540b9599a24556664d22c4e2508cf
bartoszkoziel <kozielbartosz1@gmail.com>
2023-12-19 21:12:24 +0100

    zalegly syf

--- app/script.js
+++ app/script.js
@@ -1,73 +1,85 @@
-let types = ["push", "pull", "squat", "core", "bridge", "twist", "climbing", "bike", "plank", "stretch"]
+let types = [
+  'push',
+  'pull',
+  'squat',
+  'core',
+  'bridge',
+  'twist',
+  'climbing',
+  'bike',
+  'plank',
+  'stretch',
+  'ice skating'
+]
 let currdate = Date.now()
 let workoutObj = {
-    currDate: currdate,
-    arr: []
+  currDate: currdate,
+  arr: []
 }
 
 document.body.onload = () => {
+  let btnSubmit = document.getElementById('btnSubmit')
+  let btnAdd = document.getElementById('btnAdd')
+  let btnClear = document.getElementById('btnClear')
 
-    let btnSubmit = document.getElementById('btnSubmit')
-    let btnAdd = document.getElementById('btnAdd')
-    let btnClear = document.getElementById('btnClear')
-
-    fillSelect()
-    btnSubmit.addEventListener("click", handleSubmit)
-    btnAdd.addEventListener("click", handleAdd)
-    btnClear.addEventListener("click", handleClear)
+  fillSelect()
+  btnSubmit.addEventListener('click', handleSubmit)
+  btnAdd.addEventListener('click', handleAdd)
+  btnClear.addEventListener('click', handleClear)
 }
 
 const fillSelect = () => {
-    let select = document.getElementById('selType')
+  let select = document.getElementById('selType')
 
-    types.forEach(item => {
-        let option = document.createElement("option")
-        option.setAttribute("value", item)
-        option.innerHTML = item
+  types.forEach(item => {
+    let option = document.createElement('option')
+    option.setAttribute('value', item)
+    option.innerHTML = item
 
-        select.append(option)
-    })
+    select.append(option)
+  })
 }
 
 const handleAdd = () => {
-    let type = document.getElementById("selType").value
-    let progression = document.getElementById("tbProgression").value
-    let repstime = document.getElementById("tbRepsTime").value
+  let type = document.getElementById('selType').value
+  let progression = document.getElementById('tbProgression').value
+  let repstime = document.getElementById('tbRepsTime').value
 
-    let exerciseObj = {
-        type: type,
-        progression: parseInt(progression),
-        repstime: parseInt(repstime)
-    }
+  let exerciseObj = {
+    type: type,
+    progression: parseInt(progression),
+    repstime: parseInt(repstime)
+  }
 
-    workoutObj.arr.push(exerciseObj)
+  workoutObj.arr.push(exerciseObj)
 
-    let lblExerciseArr = document.getElementById("lblExerciseArr")
-    lblExerciseArr.innerHTML = JSON.stringify(workoutObj, null, "\t")
+  let lblExerciseArr = document.getElementById('lblExerciseArr')
+  lblExerciseArr.innerHTML = JSON.stringify(workoutObj, null, '\t')
 }
 
 const handleSubmit = () => {
-    if(workoutObj.arr.length > 0) {
-        const body = JSON.stringify(workoutObj)
-        const headers = { "Content-Type": "application/json" }
-    
-        let wynik = fetch("/handleUpload", { method: 'post', body, headers })
-            .then(data => {
-                response = data.text()
-                console.log("DATA: ", data);
-                console.log("RESPONSE: ", response);
-                return response
-            })
-    
-        console.log(wynik)
-    }
+  if (workoutObj.arr.length > 0) {
+    const body = JSON.stringify(workoutObj)
+    const headers = { 'Content-Type': 'application/json' }
+
+    let wynik = fetch('/handleUpload', { method: 'post', body, headers }).then(
+      data => {
+        response = data.text()
+        console.log('DATA: ', data)
+        console.log('RESPONSE: ', response)
+        return response
+      }
+    )
+
+    console.log(wynik)
+  }
 }
 
 const handleClear = () => {
-    workoutObj = {
-        currDate: currdate,
-        arr: []
-    }
+  workoutObj = {
+    currDate: currdate,
+    arr: []
+  }
 
-    document.getElementById("lblExerciseArr").innerHTML = ""
+  document.getElementById('lblExerciseArr').innerHTML = ''
 }
--- main.js
+++ main.js
@@ -1,86 +1,80 @@
-const http = require("http")
-const fs = require("fs")
-const { log } = require("console")
-const logger = require("tracer").colorConsole()
+const http = require('http')
+const fs = require('fs')
+const { log } = require('console')
+const logger = require('tracer').colorConsole()
 
 const server = http.createServer((req, res) => {
-    if (req.method == "GET") {
-        let mainpage
-        if (req.url == "/") {
-            mainpage = fs.readFileSync('./app/index.html')
-            logger.info("GET /")
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-        }
-        else if (req.url == "/script.js") {
-            mainpage = fs.readFileSync('./app/script.js')
-            logger.info("GET /script.js")
-            res.writeHead(200, { 'Content-Type': 'text/javascript' })
-        }
-        else if (req.url == "/calendar.js") {
-            mainpage = fs.readFileSync('./app/calendar.js')
-            logger.info("GET /calendar.js")
-            res.writeHead(200, { 'Content-Type': 'text/javascript' })
-        }
-        else if (req.url == "/style.css") {
-            mainpage = fs.readFileSync('./app/style.css')
-            logger.info("GET /style.css")
-            res.writeHead(200, { 'Content-Type': 'text/css' })
-        }
-        else if (req.url == "/tracker.json") {
-            mainpage = fs.readFileSync('./tracker.json')
-            logger.info("GET /tracker.json")
-            res.writeHead(200, { 'Content-Type': 'text/css' })
-        }
-        else if (req.url == "/calendar.html") {
-            mainpage = fs.readFileSync('./app/calendar.html')
-            logger.info("GET /calendar.html")
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-        }
-        else if (req.url == "/calendar.ics") {
-            mainpage = fs.readFileSync('./app/calendar.ics')
-            logger.info("GET /calendar.ics")
-            res.writeHead(200, { 'Content-Type': 'text/plain' })
-        }
-        res.end(mainpage)
+  if (req.method == 'GET') {
+    let mainpage
+    if (req.url == '/') {
+      mainpage = fs.readFileSync('./app/index.html')
+      logger.info('GET /')
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+    } else if (req.url == '/script.js') {
+      mainpage = fs.readFileSync('./app/script.js')
+      logger.info('GET /script.js')
+      res.writeHead(200, { 'Content-Type': 'text/javascript' })
+    } else if (req.url == '/calendar.js') {
+      mainpage = fs.readFileSync('./app/calendar.js')
+      logger.info('GET /calendar.js')
+      res.writeHead(200, { 'Content-Type': 'text/javascript' })
+    } else if (req.url == '/style.css') {
+      mainpage = fs.readFileSync('./app/style.css')
+      logger.info('GET /style.css')
+      res.writeHead(200, { 'Content-Type': 'text/css' })
+    } else if (req.url == '/tracker.json') {
+      mainpage = fs.readFileSync('./tracker.json')
+      logger.info('GET /tracker.json')
+      res.writeHead(200, { 'Content-Type': 'text/css' })
+    } else if (req.url == '/calendar.html') {
+      mainpage = fs.readFileSync('./app/calendar.html')
+      logger.info('GET /calendar.html')
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+    } else if (req.url == '/calendar.ics') {
+      mainpage = fs.readFileSync('./app/calendar.ics')
+      logger.info('GET /calendar.ics')
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
     }
-    else if (req.method == "POST") {
-        if (req.url == "/handleUpload") {
-            let body = ''
-            req.on("data", (data) => {
-                body += data
-            })
+    res.end(mainpage)
+  } else if (req.method == 'POST') {
+    if (req.url == '/handleUpload') {
+      let body = ''
+      req.on('data', data => {
+        body += data
+      })
 
-            req.on("end", () => {
-                try {
-                    let post = JSON.parse(body)
-                    console.log("RECIVED: ", body)
+      req.on('end', () => {
+        try {
+          let post = JSON.parse(body)
+          console.log('RECIVED: ', body)
 
-                    res.writeHead(200, { "Content-Type": "text/plain" })
-                    res.end("ADDED EXERCISE SUCCESSFULLY!")
+          res.writeHead(200, { 'Content-Type': 'text/plain' })
+          res.end('ADDED EXERCISE SUCCESSFULLY!')
 
-                    let tracker = JSON.parse(fs.readFileSync("./tracker.json"))
-                    tracker.history.push(post)
+          let tracker = JSON.parse(fs.readFileSync('./tracker.json'))
+          tracker.history.push(post)
 
-                    fs.writeFileSync("./tracker.json", JSON.stringify(tracker, null, 4), "utf-8")
-                    
-                    return
-                }
-                catch (err) {
-                    console.log("tu: ", err)
-                    res.writeHead(500, { "Content-Type": "text/plain" })
-                    res.write("Bad Post Data.  Is your data a proper JSON?\n")
-                    res.end()
-                    return
-                }
-            })
+          fs.writeFileSync(
+            './tracker.json',
+            JSON.stringify(tracker, null, 4),
+            'utf-8'
+          )
+
+          return
+        } catch (err) {
+          console.log('tu: ', err)
+          res.writeHead(500, { 'Content-Type': 'text/plain' })
+          res.write('Bad Post Data.  Is your data a proper JSON?\n')
+          res.end()
+          return
         }
+      })
     }
+  }
 })
 
 server.listen(3000, () => {
-    console.log("running on port 3000")
+  console.log('running on port 3000')
 })
 
-const genIcs = (body) => {
-
-}
+const genIcs = body => {}
