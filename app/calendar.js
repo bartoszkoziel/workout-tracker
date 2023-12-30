bbc1ec580d503aa3ec870b1d925896363f497d08
bartoszkoziel <kozielbartosz1@gmail.com>
2023-12-30 21:27:30 +0100

    cos rozjebalem

--- app/calendar.js
+++ app/calendar.js
@@ -1,23 +1,20 @@
 document.body.onload = async () => {
-  let trackerObj = await fetchTracker()
-  trackerObj = trackerObj.history
-  console.log(trackerObj)
-
-  trackerObj.forEach(async el => {
-    el.currDate = parseInt(el.currDate.toString().slice(0, -3))
-    let week = epochToWeek(el.currDate)
-    drawProgress(week)
-  })
+  todo()
+  document.getElementById("btnShow").addEventListener("click", todo)
 }
 
-function drawProgress (week) {
+function drawProgress(week) {
   // 1672534800 -> 1.1.23
   // 1704063539 -> 31.12.23
   //   31528739 -> one year
   //     604800 -> one week
+
+  if (week == -1) {
+    return 0
+  }
+
   week = document.getElementById(week)
   let filling = document.createElement('div')
-  // let currentHeight = parseInt(filling.style.height.slice(0, -2))
 
   filling.className = 'filling'
   filling.style.height += 15 + 'px'
@@ -25,23 +22,28 @@ function drawProgress (week) {
   week.append(filling)
 }
 
-function epochToWeek (epoch) {
+function epochToWeek(epoch) {
   // Get the current year from the browser
-  const currentYear = new Date().getFullYear()
-  // Create a new Date object for January 1st of the current year
-  const januaryFirst = new Date(currentYear, 0, 1)
+  const currentYear = document.getElementById("tbYear").value
   // Get the timestamp (epoch time) of January 1st
-  const januaryFirstTimestamp = januaryFirst.getTime()
+  const januaryFirst = new Date(currentYear, 0, 1)
+  // Get the timestamp of December 31st
+  const decemberLast = new Date(currentYear, 11, 31)
+
 
   let firstYearDay = parseInt(januaryFirst.getTime().toString().slice(0, -3))
+  let lastYearDay = parseInt(decemberLast.getTime().toString().slice(0, -3))
 
-  let yearDiff = epoch - firstYearDay
-  let weekSec = 604800
+  if (epoch > firstYearDay && epoch < lastYearDay) {
+    let yearDiff = epoch - firstYearDay
+    let weekSec = 604800
+    return Math.floor(yearDiff / weekSec) + 1
+  }
+  return -1
 
-  return Math.floor(yearDiff / weekSec) + 1
 }
 
-async function fetchTracker () {
+async function fetchTracker() {
   try {
     // Fetch data from the /tracker.json endpoint
     const response = await fetch('/tracker.json')
@@ -58,4 +60,15 @@ async function fetchTracker () {
   } catch (error) {
     console.error('Error fetching data:', error.message)
   }
+}
+
+async function todo() {
+  let trackerObj = await fetchTracker()
+  trackerObj = trackerObj.history
+  console.log(trackerObj)
+  trackerObj.forEach(async el => {
+    el.currDate = parseInt(el.currDate.toString().slice(0, -3))
+    let week = epochToWeek(el.currDate)
+    drawProgress(week)
+  })
 }
